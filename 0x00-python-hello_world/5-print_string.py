cc9017494ce5648cadcaaf83a717c33484580dcb
mubarekmuhammed <mubemuhammed0985@gmail.com>
2022-06-06 23:40:02 +0300

    Update 5-print_string.py

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 str = "Holberton School"
 print(3 * str)
-print("\n")
+print(str[:0])
+print(str[:1])
+print(str[:2])
+print(str[:3])
+print(str[:4])
+print(str[:5])
+print(str[:6])
+print(str[:7])
+print(str[:8])
 print(str[:9])
-print("\n")
