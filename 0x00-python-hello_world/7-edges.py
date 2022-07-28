d9e2032de0b47f4646ec648443e3b0a33942cedc
mubarekmuhammed <mubemuhammed0985@gmail.com>
2022-07-28 11:16:39 +0300

    Update 7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-f="Hol"
-d="on"
-c="olberto"
+f=word[0:3]
+d=word[-2:]
+c=word[1:-1]
 print(f"First 3 letters: {f}")
 print(f"Last 2 letters: {d}")
 print(f"Middle word: {c}")
