3f1d38f13a285f7bdd0459e7765b6577a84b6a27
mubarekmuhammed <mubemuhammed0985@gmail.com>
2022-07-28 11:26:19 +0300

    update cut and copy

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-f=word[0:3]
-d=word[-2:]
-c=word[1:-1]
+f = word[0:3]
+d = word[-2:]
+c = word[1:-1]
 print("First 3 letters: {}".format(f))
 print("Last 2 letters: {}".format(d))
 print("Middle word: {}".format(c))
